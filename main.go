10d3a6e203f2fbe89bf04a4e8218677626ce4da3
TheKankan <quentin.lejuez@gmail.com>
2025-09-12 13:08:26 +0200

    Basic REPL

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break // exit on EOF or error
+		}
+		line := scanner.Text() // get current line
+		words := cleanInput(line)
+		if len(words) == 0 {
+			fmt.Printf("No input provided \n\n")
+			continue // ignore empty input
+		}
+		fmt.Printf("Your command was: %s\n\n", words[0])
+	}
 }
 
 func cleanInput(text string) []string {
